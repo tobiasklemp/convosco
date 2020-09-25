1d6a6f906c0d9feed9598ff7763a5530e22ad512
Tobias Klemp <tklemp@gmx.de>
2020-09-25 12:51:55 +0200

    - setup appswitch

--- apps/demo/src/app.ts
+++ apps/demo/src/app.ts
@@ -1,3 +1,8 @@
+import { setupBraintreeAppDeligate } from '@convosco/nativescript-braintree';
 import { Application } from '@nativescript/core';
+
+if (Application.ios) {
+    setupBraintreeAppDeligate("org.nativescript.demo.payments");
+}
 
 Application.run({ moduleName: 'app-root' });
